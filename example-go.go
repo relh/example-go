2a614fcf0947d4864c9bd8f11011a87f9bcb71ae
Richard Higgins <richard@disys.io>
2015-05-10 12:49:48 -0700

    no package seems the answer

--- example-go.go
+++
@@ -1,6 +0,0 @@
-package main // import "app"
-
-import "fmt"
-func main() {
-    fmt.Println("hello world")
-}
---
+++ main.go
@@ -0,0 +1,9 @@
+// this is not sanctioned
+
+package main 
+
+import  "fmt"
+
+func main() {
+    fmt.Println("hello world")
+}
